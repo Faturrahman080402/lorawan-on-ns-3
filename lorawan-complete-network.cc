873e07794524f5201dec03b0eff30b2fd0e7ebe9
Fatur Rahman <125888033+Faturrahman080402@users.noreply.github.com>
2025-01-04 17:53:29 +0700

    change

--- lorawan-complete-network.cc
+++ lorawan-complete-network.cc
@@ -190,6 +190,48 @@ int main(int argc, char *argv[])
     Ptr<LoraDeviceAddressGenerator> addrGen =
         CreateObject<LoraDeviceAddressGenerator>(nwkId, nwkAddr);
 
+    /*
+   // Membuat channel
+Ptr<LogDistancePropagationLossModel> loss = CreateObject<LogDistancePropagationLossModel>();
+loss->SetPathLossExponent(3.76);
+loss->SetReference(1, 7.7);
+
+Ptr<PropagationDelayModel> delay = CreateObject<ConstantSpeedPropagationDelayModel>();
+Ptr<Channel> channel = CreateObject<PropagationLossPropagationDelayModel>(loss, delay);
+
+// Membuat LoraHelper
+LoraHelper loraHelper;
+LoraPhyHelper phyHelper = LoraPhyHelper();
+LorawanMacHelper macHelper = LorawanMacHelper();
+
+// Menghubungkan channel ke PHY helper
+phyHelper.SetChannel(channel);
+
+// Atur MAC LoRa sebagai End Device
+macHelper.SetDeviceType(LorawanMacHelper::ED);
+
+// Instal PHY dan MAC pada end devices
+loraHelper.Install(phyHelper, macHelper, endDevices);
+
+// Instal network server
+NetworkServerHelper networkServerHelper;
+networkServerHelper.Install(networkServer);
+
+// Instal forwarder pada gateway
+ForwarderHelper forwarderHelper;
+forwarderHelper.Install(gateways);
+
+// Tambahkan aplikasi pada end devices
+PeriodicalSenderHelper appHelper = PeriodicalSenderHelper();
+appHelper.SetPeriod(Seconds(10));  // Perangkat mengirim data setiap 10 detik
+appHelper.Install(endDevices);
+
+// Jalankan simulasi
+Simulator::Run();
+Simulator::Destroy();
+
+    */
+
     // Create the LoraNetDevices of the end devices
     macHelper.SetAddressGenerator(addrGen);
     phyHelper.SetDeviceType(LoraPhyHelper::ED);
